7cf3c4917ceab1d8a23d2ae47f1a3d956304fb72
dykai <yingkaidang@gmail.com>
2019-06-01 17:34:08 +0800

    整理Script目录

---
+++ Assets/Scripts/Scene/Replay/UI/StateBoard.cs
@@ -0,0 +1,35 @@
+﻿using Simuro5v5;
+using UnityEngine;
+using TMPro;
+
+public class StateBoard : MonoBehaviour
+{
+    public TextMeshProUGUI stateText;
+
+    public void Render(MatchPhase mp)
+    {
+        switch (mp)
+        {
+            case MatchPhase.FirstHalf:
+                {
+                    stateText.text = "First Half";
+                    break;
+                }
+            case MatchPhase.SecondHalf:
+                {
+                    stateText.text = "Second Half";
+                    break;
+                }
+            case MatchPhase.OverTime:
+                {
+                    stateText.text = "Over Time";
+                    break;
+                }
+            case MatchPhase.Penalty:
+                {
+                    stateText.text = "Penalty Shootout";
+                    break;
+                }
+        }
+    }
+}
