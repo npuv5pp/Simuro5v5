020e8b9da59f95afea48a55f3de26648d4c81185
pg999w <pg999w@outlook.com>
2019-07-14 15:36:05 +0800

    添加生成脚本

---
+++ Assets/Plugins/Editor/Build.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildBatch
+{
+    private static void BuildGame(BuildTarget target, string path = null)
+    {
+        string truePath = path ?? EditorUtility.SaveFolderPanel("Choose Build Location", "", "");
+        var scenes =
+            EditorBuildSettings.scenes
+                .Where(s => s.enabled)
+                .Select(s => s.path).ToArray();
+        var options = new BuildOptions();
+        BuildPipeline.BuildPlayer(scenes, truePath, target, options);
+    }
+    
+    [MenuItem("File/Batch Build/Win32")]
+    public static void BuildWindows32()
+    {
+        BuildGame(BuildTarget.StandaloneWindows);
+    }
+    
+    [MenuItem("File/Batch Build/Win64")]
+    public static void BuildWindows64()
+    {
+        BuildGame(BuildTarget.StandaloneWindows64);
+    }
+
+    [MenuItem("File/Batch Build/Win32 and Win64")]
+    public static void BuildWindows32And64()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Build Location", "", "");
+        string win32Path = Path.Combine(path, "win32", "Simuro5v5.exe");
+        string win64Path = Path.Combine(path, "win64", "Simuro5v5.exe");
+        BuildGame(BuildTarget.StandaloneWindows, win32Path);
+        BuildGame(BuildTarget.StandaloneWindows64, win64Path);
+    }
+}
