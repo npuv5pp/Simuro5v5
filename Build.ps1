1250f49a02f0856f03e6dc4b69bd4020c7373131
pg999w <pg999w@outlook.com>
2019-07-15 11:58:42 +0800

    使编译脚本支持 VS 2019

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,10 @@
 ﻿#Created by AzureFx on 2019/4/19
 
 $msbuild = (Get-Command 'MSBuild.exe' -ErrorAction Ignore).Source
-if ($null -eq $msbuild) {
-    $msbuild = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community\MSBuild" -Recurse -File 'MSBuild.exe' | Where-Object { $_.Directory.Name -eq 'Bin' } | Sort-Object -Property LastWriteTime | Select-Object -Last 1).FullName
+foreach ($vsver in 2017,2019) {
+    if ($null -eq $msbuild) {
+        $msbuild = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\$vsver\Community\MSBuild" -Recurse -File 'MSBuild.exe' | Where-Object { $_.Directory.Name -eq 'Bin' } | Sort-Object -Property LastWriteTime | Select-Object -Last 1).FullName
+    }
 }
 if ($null -eq $msbuild) {
     $msbuild = Read-Host -Prompt '请提供 MSBuild.exe 可执行程序的路径'
