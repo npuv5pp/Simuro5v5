d63f8f7a7af3403ef7b9b0a2e4800b50762a7c7b
Peng Guanwen <pg999w@outlook.com>
2020-05-08 15:34:14 +0800

    Make Build.ps1 support Linux

--- Build.ps1
+++ Build.ps1
@@ -1,13 +1,17 @@
 ﻿#Created by AzureFx on 2019/4/19
 
-$msbuild = (Get-Command 'MSBuild.exe' -ErrorAction Ignore).Source
-foreach ($vsver in 2017,2019) {
-    if ($null -eq $msbuild) {
-        $msbuild = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\$vsver\Community\MSBuild" -Recurse -File 'MSBuild.exe' | Where-Object { $_.Directory.Name -eq 'Bin' } | Sort-Object -Property LastWriteTime | Select-Object -Last 1).FullName
+if ($IsLinux) {
+    $msbuild = (Get-Command 'dotnet' -ErrorAction Ignore).Source
+} else {
+    $msbuild = (Get-Command 'MSBuild.exe' -ErrorAction Ignore).Source
+    foreach ($vsver in 2017,2019) {
+        if ($null -eq $msbuild) {
+            $msbuild = (Get-ChildItem -Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\$vsver\Community\MSBuild" -Recurse -File 'MSBuild.exe' | Where-Object { $_.Directory.Name -eq 'Bin' } | Sort-Object -Property LastWriteTime | Select-Object -Last 1).FullName
+        }
     }
 }
 if ($null -eq $msbuild) {
-    $msbuild = Read-Host -Prompt '请提供 MSBuild.exe 可执行程序的路径'
+    $msbuild = Read-Host -Prompt '请提供 MSBuild.exe / dotnet 可执行程序的路径'
 }
 $msbuild = Resolve-Path $msbuild -ErrorAction Stop
 
@@ -16,5 +20,9 @@ $outputPath = '.\Assets\BinDeps\V5RPC\'
 New-Item -Path $outputPath -ItemType Directory -ErrorAction Ignore | Out-Null
 $outputPath = Resolve-Path $outputPath -ErrorAction Stop
 $escapedPath = '"' + ($outputPath -replace '\\', '/') + '"'
-&$msbuild '/t:Restore;Build' "/p:Configuration=Release;OutputPath=$escapedPath" $sln
+if ($msbuild -match 'dotnet$') {
+    &$msbuild build --configuration Release --output $escapedPath $sln
+} else {
+    &$msbuild '/t:Restore;Build' "/p:Configuration=Release;OutputPath=$escapedPath" $sln
+}
 Get-ChildItem -Path $outputPath | Where-Object { $_.Extension -in '.pdb', '.json' } | Remove-Item
